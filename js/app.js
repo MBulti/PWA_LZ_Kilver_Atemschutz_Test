e20a2744d97069aead139e864147c00806f36265
MBulti <m.bulthaup.development@gmail.com>
2021-07-04 21:01:49 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 if('serviceWorker' in navigator){
+    console.log("service worker is supported");
     navigator.serviceWorker.register('./sw.js')
       .then(reg => console.log('service worker registered', reg))
       .catch(err => console.log('service worker not registered', err));
