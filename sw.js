d5587eee29d2591c020340a6eab0d87992a46e66
MBulti <m.bulthaup.development@gmail.com>
2021-07-04 21:32:46 +0200

    offline support

--- sw.js
+++ sw.js
@@ -1,13 +1,35 @@
+const staticCacheName = "site-static";
+const assets = [
+    "./",
+    "./index.html",
+    "./js/app.js",
+    "./js/ui.js",
+    "./js/materialize.min.js",
+    "./css/styles.css",
+    "./css/materialize.min.css",
+    "./img/default.png",
+    "https://fonts.googleapis.com/icon?family=Material+Icons",
+    "https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2"
+];
+
 // install service worker
-self.addEventListener('install', event => {
-    console.log(event);
-    console.log("installed");
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            console.log("caching assets");
+            cache.addAll(assets);
+        })
+    );
 });
 // activate service worker
-self.addEventListener('activate', event => {
+self.addEventListener("activate", event => {
     console.log("serviceworker is active");
 });
 // fetch event
-self.addEventListener('fetch', event => {
-    console.log('fetch event', event);
-})
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(cacheRes => {
+            return cacheRes || fetch(event.request); // cache first, network second
+        })
+    );
+});
