34e524ff824ae3e938225d07274ef959089fb1b5
MBulti <m.bulthaup.development@gmail.com>
2021-07-06 08:23:00 +0200

    added sidenav

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = "site-static-v3"; // remember to update this if any file is changed
-const dynamicCacheName = "site-dynamic-v3";
+const staticCacheName = "site-static-v4"; // remember to update this if any file is changed
+const dynamicCacheName = "site-dynamic-v4";
 const assets = [
     "./",
     "./index.html",
